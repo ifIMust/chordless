b3cdd75484441fb4051ca3075456ff78e53714e6
Trent <why@no.com>
2024-07-23 11:51:25 -0600

    refine test with separated mock and fixture

--- note/test_full_voicing_observer.cc
+++ note/test_full_voicing_observer.cc
@@ -1,59 +1,50 @@
 #include "full_voicing_observer.h"
 
 #include "note_state.h"
-#include "../ui/text_setter.h"
+#include "../ui/mock/mock_text_setter.h"
 
 #include <gtest/gtest.h>
 #include <string>
 
-class MockTextSetter : public chordless::ui::TextSetter {
+class FullVoicingObserverTest : public testing::Test {
 public:
-  void SetText(const std::string &text) override { text_ = text; }
-  std::string text_;
+  FullVoicingObserverTest() : observer(note_state, text_setter) {}
+  
+protected:
+  chordless::ui::MockTextSetter text_setter;
+  chordless::note::NoteState note_state;
+  chordless::note::FullVoicingObserver observer;
 };
 
-TEST(FullVoicingObserverTest, Empty) {
-  MockTextSetter text_setter;
-  chordless::note::NoteState note_state;
-  chordless::note::FullVoicingObserver o(note_state,text_setter);
-  o.Observe();
+TEST_F(FullVoicingObserverTest, Empty) {
+  observer.Observe();
   EXPECT_TRUE(text_setter.text_.empty());
 }
 
-TEST(FullVoicingObserverTest, OneNote) {
-  MockTextSetter text_setter;
-  chordless::note::NoteState note_state;
-  chordless::note::FullVoicingObserver o(note_state,text_setter);
+TEST_F(FullVoicingObserverTest, OneNote) {
   std::string expected("C ");
   note_state.NoteOn(0);
-  o.Observe();
+  observer.Observe();
   EXPECT_EQ(expected, text_setter.text_);
 }
 
-TEST(FullVoicingObserverTest, ThreeNote) {
-  MockTextSetter text_setter;
-  chordless::note::NoteState note_state;
-  chordless::note::FullVoicingObserver o(note_state,text_setter);
+TEST_F(FullVoicingObserverTest, ThreeNote) {
   std::string expected("C E G ");
   note_state.NoteOn(0);
   note_state.NoteOn(16);
   note_state.NoteOn(31);
-  o.Observe();
+  observer.Observe();
   EXPECT_EQ(expected, text_setter.text_);
 }
 
-
-TEST(FullVoicingObserverTest, Reobserve) {
-  MockTextSetter text_setter;
-  chordless::note::NoteState note_state;
-  chordless::note::FullVoicingObserver o(note_state,text_setter);
+TEST_F(FullVoicingObserverTest, Reobserve) {
   std::string expected("C G ");
   note_state.NoteOn(0);
   note_state.NoteOn(16);
   note_state.NoteOn(31);
-  o.Observe();
+  observer.Observe();
 
   note_state.NoteOff(16);
-  o.Observe();
+  observer.Observe();
   EXPECT_EQ(expected, text_setter.text_);
 }
